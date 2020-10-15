0c8699dcfbd5400ce2f7d705759803cd5eb52e17
raph <raphaelsydelacruz@gmail.com>
2020-10-15 17:50:05 -0400

    Added code haha whats up

---
+++ question1.py
@@ -0,0 +1,13 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+
+pkl_file = 'q1_dataset.pkl'
+data = pickle.load(open(pkl_file, 'rb'))
+data = np.array(data)
+X = data[:, :2]
+y = data[:, -1]
+plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
+plt.show()
