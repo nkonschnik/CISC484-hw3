dacb6e4df71b8625f629e50096e23ce488bc9057
Ahmad Tamimi <tamimoz@udel.edu>
2020-10-17 06:50:10 +0300

    Fixed variable names and answered first part of q2

--- question2.py
+++ question2.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.metrics import confusion_matrix
+import time
 
 pkl_file = 'hw3-data/hw3-data/q2_training.pkl'
 data = pickle.load(open(pkl_file, 'rb'))
@@ -13,40 +14,38 @@ X = data[:, :2]
 Y = data[:, -1]
 
 pkl_file_test = 'hw3-data/hw3-data/q2_test.pkl'
-dataTest = pickle.load(open(pkl_file, 'rb'))
-dataTest = np.array(data)
-XTest = data[:, :2]
-YTest = data[:, -1]
+dataTest = pickle.load(open(pkl_file_test, 'rb'))
+dataTest = np.array(dataTest)
+XTest = dataTest[:, :2]
+YTest = dataTest[:, -1]
 
 
-print("\nKernel = linear: ")
+print("Kernel = linear: ")
 
 clfLinear = svm.SVC(C=10, kernel='linear')
-clfLinear.fit(X,Y)
-print("Accuracy " + str(clfLinear.fit(X,Y).score(X,Y)))
+linearFit = clfLinear.fit(X,Y)
 
-differenceListLinear = np.subtract(clfLinear.fit(X,Y).predict(XTest), YTest)
+differenceListLinear = np.subtract(linearFit.predict(XTest), YTest)
 
 
 errorCounterLinear = 0
 for i in range(len(differenceListLinear)):
     if differenceListLinear[i] != 0:
         errorCounterLinear += 1
-print("Number of misclassifications: " + str(errorCounterLinear))
+print("    Number of misclassifications: " + str(errorCounterLinear))
+print("    Accuracy " + str(linearFit.score(XTest,YTest)))
 
 
 print("\n\nKernel = rbf: ")
 clfRBF = svm.SVC(C=10, kernel='rbf')
-clfRBF.fit(X,Y)
-print("Accuracy " + str(clfRBF.fit(X,Y).score(X,Y)))
+RBFfit = clfRBF.fit(X,Y)
 
-
-differenceListRBF = np.subtract(clfRBF.fit(X,Y).predict(XTest), YTest)
-
+differenceListRBF = np.subtract(RBFfit.predict(XTest), YTest)
 
 errorCounterRBF = 0
 for i in range(len(differenceListRBF)):
     if differenceListRBF[i] != 0:
         errorCounterRBF += 1
 
-print("Number of misclassifications: " + str(errorCounterRBF))
+print("    Number of misclassifications: " + str(errorCounterRBF))
+print("    Accuracy " + str(RBFfit.score(XTest,YTest)))
