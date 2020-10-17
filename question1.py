fd0961bd4255dc2464f139dafcfc698a5468c74c
Ahmad Tamimi <tamimoz@udel.edu>
2020-10-17 18:58:14 +0300

    Added some initial setup to determine w and b for q1

--- question1.py
+++ question1.py
@@ -14,27 +14,78 @@ Y = data[:, -1]
 plt.scatter(X[:, 0], X[:, 1], c=Y, s=30, cmap=plt.cm.Paired)
 plt.savefig('q1_graph')
 
-print("\nC=1: ")
+fignum = 1
 
-clf = svm.SVC()
-clf.fit(X,Y)
-print(clf.fit(X,Y).score(X,Y))
-print(clf.fit(X,Y).dual_coef_)
-print(clf.fit(X,Y).intercept_)
+for name, penalty in (('case1',1),('case2',3)):
+    
+    print("\nC=" + str(penalty) + ": ")
+    clf = svm.SVC(kernel = 'linear', C=penalty)
+    clf.fit(X,Y)
+    print(clf.fit(X,Y).score(X,Y))
+    #print(clf.fit(X,Y).dual_coef_)
+    #print(clf.fit(X,Y).intercept_)
 
-difference_list_c1 = np.subtract(clf.predict(X), Y)
+    difference_list_c1 = np.subtract(clf.predict(X), Y)
 
-isError = False
+    isError = False
 
-for i in range(len(difference_list_c1)):
-    if difference_list_c1[i] != 0:
-        if not(isError):
-            print("\nError instances:")
-            isError = True
-        print("    -" + str(X[i]))
+    for i in range(len(difference_list_c1)):
+        if difference_list_c1[i] != 0:
+            if not(isError):
+                print("\nError instances:")
+                isError = True
+            print("    -" + str(X[i]))
 
+    print(clf.support_vectors_)
+    
+    # get the separating hyperplane
+    w = clf.dual_coef_[0]
+    a = -w[0] / w[1]
+    xx = np.linspace(-5, 5)
+    yy = a * xx - (clf.intercept_[0]) / w[1]
 
+    # plot the parallels to the separating hyperplane that pass through the
+    # support vectors (margin away from hyperplane in direction
+    # perpendicular to hyperplane). This is sqrt(1+a^2) away vertically in
+    # 2-d.
+    margin = 1 / np.sqrt(np.sum(clf.coef_ ** 2))
+    yy_down = yy - np.sqrt(1 + a ** 2) * margin
+    yy_up = yy + np.sqrt(1 + a ** 2) * margin
 
+    # plot the line, the points, and the nearest vectors to the plane
+    plt.figure(fignum, figsize=(4, 3))
+    plt.clf()
+    plt.plot(xx, yy, 'k-')
+    plt.plot(xx, yy_down, 'k--')
+    plt.plot(xx, yy_up, 'k--')
+
+    plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80,
+                facecolors='none', zorder=10, edgecolors='k')
+    plt.scatter(X[:, 0], X[:, 1], c=Y, zorder=10, cmap=plt.cm.Paired,
+                edgecolors='k')
+
+    plt.axis('tight')
+    x_min = -4.8
+    x_max = 4.2
+    y_min = -6
+    y_max = 6
+
+    XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
+    Z = clf.predict(np.c_[XX.ravel(), YY.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(XX.shape)
+    plt.figure(fignum, figsize=(4, 3))
+    plt.pcolormesh(XX, YY, Z, cmap=plt.cm.Paired)
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    plt.xticks(())
+    plt.yticks(())
+    fignum = fignum + 1
+plt.savefig("q1_plot")
+"""
 print("\n\nC=3: ")
 clf3 = svm.SVC(C=3)
 clf3.fit(X,Y)
@@ -53,3 +104,4 @@ for i in range(len(difference_list_c3)):
             print("\nError instances:")
             isError = True
         print("    -" + str(X[i]))
+"""
